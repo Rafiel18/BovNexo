90bf64f9b4ece43617e2c3fe201ed5910d9c90b7
Rafiel18 <rafaelroliveira37@gmail.com>
2026-05-08 16:45:17 -0300

    Atualizar o App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 import { useEffect, useState } from "react";
 import AuthPage from "./pages/AuthPage";
+import LandingPage from "./pages/LandingPage";
 import ProducerDashboard from "./pages/ProducerDashboard";
 import VetDashboard from "./pages/VetDashboard";
+
 import { onAuthChange, signOutUser } from "./services/supabaseAuth";
 import { getProfileById } from "./services/profile";
 
@@ -9,6 +11,7 @@ function App() {
   const [authUser, setAuthUser] = useState(null);
   const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [showAuth, setShowAuth] = useState(false);
 
   useEffect(() => {
     const unsubscribe = onAuthChange(async (user) => {
@@ -21,7 +24,9 @@ function App() {
 
       try {
         setAuthUser(user);
+
         const profile = await getProfileById(user.id);
+
         setUserData(profile);
       } catch (error) {
         console.error("Erro ao buscar perfil:", error);
@@ -36,38 +41,59 @@ function App() {
 
   async function handleLogout() {
     await signOutUser();
+
     setAuthUser(null);
     setUserData(null);
+
+    // volta para landing ao sair
+    setShowAuth(false);
   }
 
   if (loading) {
     return (
-      <div className="min-h-screen bg-zinc-100 flex items-center justify-center">
+      <div className="min-h-screen flex items-center justify-center bg-zinc-100">
         <p className="text-lg text-zinc-600">Carregando...</p>
       </div>
     );
   }
 
+  // Usuário não autenticado
   if (!authUser || !userData) {
+    if (!showAuth) {
+      return (
+        <LandingPage
+          onStart={() => setShowAuth(true)}
+        />
+      );
+    }
+
     return <AuthPage />;
   }
 
+  // Dashboard produtor
   if (userData.role === "produtor") {
-    return <ProducerDashboard userData={userData} onLogout={handleLogout} />;
-  }
-
-  if (userData.role === "veterinario") {
     return (
-     <VetDashboard
-  userData={userData}
-  authUser={authUser}
-  onLogout={handleLogout}
-/>
+      <ProducerDashboard
+        userData={userData}
+        onLogout={handleLogout}
+      />
     );
   }
 
+  // Dashboard veterinário
+  if (userData.role === "veterinario") {
+    return (
+      <VetDashboard
+        userData={userData}
+        authUser={authUser}
+        onLogout={handleLogout}
+      />
+    );
+  }
+
+  // fallback
   return (
-    <div className="min-h-screen bg-zinc-100 flex items-center justify-center">
+    <div className="min-h-screen flex items-center justify-center bg-zinc-100">
       <p className="text-lg text-red-600">
         Tipo de usuário inválido: {String(userData.role)}
       </p>
